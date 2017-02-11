1c00adbee82d69c7d2c8f210eb536df0a006a3ca
hasegawatomoki <github@speedstars.jp>
2017-02-11 13:16:40 +0900

    Chapter 06-01

---
+++ GourmetSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GourmetSearch
+//
+//  Created by 長谷川智希 on 2017/02/11.
+//  Copyright © 2017年 長谷川智希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GourmetSearch' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GourmetSearch
+  pod 'SDWebImage'
+
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+end
